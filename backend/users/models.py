9ee62ac0cbab2b0263f55cc240d45b8d2c09ae10
Kernyxon <Kernyxon@users.noreply.github.com>
2026-04-27 18:42:18 +0300

    add users app

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    USERNAME_FIELD = 'email'
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+menu = [
+        {'title': 'Home', 'url': 'home'},
+        {'title': 'Login', 'url': 'login'},
+        {'title': 'Register', 'url': 'register'},
+        ]
+
+def index(request):
+    context = {
+            'menu': menu,
+            }
+    return render(request, 'core/home.html', context=context)
