fbe987c03197470e4376464922a01973a364bedb
Kernyxon <Kernyxon@users.noreply.github.com>
2026-04-27 18:38:54 +0300

    add authorize app

--- backend/authorize/views.py
+++ backend/authorize/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+menu = [
+        {'title': 'Home', 'url': 'home'},
+        {'title': 'Login', 'url': 'login'},
+        {'title': 'Register', 'url': 'register'},
+        ]
+
+def login(request):
+    context = {
+            'menu': menu,
+            'title': 'Login',
+            }
+    return render(request, 'authorize/login.html', context=context)
+
+def register(request):
+    context = {
+            'menu': menu,
+            'title': 'Register',
+            }
+    return render(request, 'authorize/register.html', context=context)
