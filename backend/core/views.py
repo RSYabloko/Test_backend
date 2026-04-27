4eb8da85dcaf23e120f81bc2e790da5a018151c7
Kernyxon <Kernyxon@users.noreply.github.com>
2026-04-27 18:29:52 +0300

    add template

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+menu = [
+        {'title': 'Home', 'url': 'home'},
+        {'title': 'Login', 'url': 'login'},
+        {'title': 'Register', 'url': 'register'},
+        ]
+
+def home(request):
+    context = {
+            'menu': menu,
+            'title': 'Home',
+            }
+    return render(request, 'core/home.html', context=context)
