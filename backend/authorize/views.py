26ea8bcb1c7b56cbb8b6a8bd84210cb71d2e477f
Kernyxon <Kernyxon@users.noreply.github.com>
2026-04-29 20:16:59 +0300

    add function register

--- backend/authorize/views.py
+++ backend/authorize/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 menu = [
         {'title': 'Home', 'url': 'home'},
@@ -18,4 +22,32 @@ def register(request):
             'menu': menu,
             'title': 'Register',
             }
+    if request.method == 'POST':
+        email = request.POST.get('Email')
+        first_name = request.POST.get('First name')
+        middle_name = request.POST.get('Middle name')
+        last_name = request.POST.get('Last name')
+        password = request.POST.get('Password')
+        confirm_password = request.POST.get('Confirm password')
+
+        if len(password) < 8:
+            return render(request, 'authorize/register.html', {'menu': menu, 'title': 'Register', 'error': 'Password must be least 8 simbols'})
+
+        if password != confirm_password:
+            return render(request, 'authorize/register.html', {'menu': menu, 'title': 'Register', 'error': 'Password error'})
+
+        if User.objects.filter(email=email).exists():
+            return render(request, 'authorize/register.html', {'menu': menu, 'title': 'Register', 'error': 'User exists'})
+
+        user = User.objects.create(
+                email=email,
+                first_name=first_name,
+                middle_name=middle_name,
+                last_name=last_name
+            )
+        user.set_password(password)
+        user.save()
+
+        messages.success(request, 'Register successful')
+
     return render(request, 'authorize/register.html', context=context)
